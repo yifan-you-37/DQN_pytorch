97a25fa427dc40c7d277a10420c7de50956b9466
Yifan You <harry47341@Yahoo.com>
2020-08-09 22:29:00 +0000

    exp script

---
+++ exp_script.sh
@@ -0,0 +1,2 @@
+python main.py --env VentureNoFrameskip-v4 --grac --exp_name exp_atari --comment one_q_one_a
+python main.py --env YarsRevengeNoFrameskip-v4 --grac --exp_name exp_atari --comment one_q_one_a
